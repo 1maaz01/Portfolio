32a7652c04173d87504ae1748d6a48152c8c068c
1maaz01 <mmrayeen401@gmail.com>
2025-04-10 23:06:03 +0530

    Add files via upload

---
+++ src/components/cards/ProjectCard.jsx
@@ -0,0 +1,214 @@
+import React from "react";
+import styled from "styled-components";
+
+
+/*const CardContainer = styled.div`
+  display: flex; 
+  gap: 20px; 
+  justify-content: flex-start;
+  padding: 10px;
+  height: 100vh;
+  width : 100vw;
+  align-items: center; 
+
+  @media only screen and (max-width: 1000px) {
+    width: 100vw;
+    height: 100vh;
+    display : block;
+  `
+*/
+
+
+
+const CardContainer = styled.div`
+  display: flex; /* Ensure cards are displayed side by side */
+  gap: 50px; /* Space between the cards */
+  justify-content: flex-start; /* Align the cards from the left side */
+  padding: 100px;
+  height: 90vh; /* Full viewport height */
+  width: 100vw; /* Full viewport width */
+  justify-content: center;
+  align-items: center; /* Vertically center the cards within the container */
+
+  @media only screen and (max-width: 1000px) {
+    width: 100vw; /* Full viewport width */
+    height: 100vh; /* Full viewport height */
+    display: flex; /* Keep flex for consistency, or change to block if stacking vertically */
+    flex-direction: column; /* Stack cards vertically on smaller screens */
+    padding: 10px;
+  }
+`;
+
+
+
+
+const Card1 = styled.div`
+  width: 60vw; /* Set width to 60% for Card1 */
+  height: 80vh; /* Set height to 60% */
+  background-color: ${({ theme }) => theme.card};
+  cursor: pointer;
+  border-radius: 10px;
+  box-shadow: 0 0 12px 4px rgba(0, 0, 0, 0.4);
+  overflow: hidden;
+  padding: 26px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    transform: translateY(-10px);
+    box-shadow: 0 0 50px 4px rgba(0, 0, 0, 0.6);
+    filter: brightness(1.1);
+  }
+  @media only screen and (max-width: 1000px) {
+    width: 100vw;
+    height: 60vh;
+`;
+
+const Card2 = styled.div`
+  width: 40vw; /* Set width to 40% for Card2 */
+  height: 80vh; /* Set height to 60% */
+  background-color: ${({ theme }) => theme.card};
+  cursor: pointer;
+  border-radius: 10px;
+  box-shadow: 0 0 12px 4px rgba(0, 0, 0, 0.4);
+  overflow:hidden;
+  padding: 26px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  transition: all 0.5s ease-in-out;
+
+  &:hover {
+    transform: translateY(-10px);
+    box-shadow: 0 0 50px 4px rgba(0, 0, 0, 0.6);
+    filter: brightness(1.1);
+  }
+
+    @media only screen and (max-width: 1000px) {
+    width: 100vw;
+    height: 40vh;
+
+`;
+
+
+
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.white};
+  border-radius: 10px;
+  box-shadow: 0 0 16px 2px rgba(0, 0, 0, 0.3);
+`;
+const Tags = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 8px;
+  margin-top: 4px;
+  color:purple;
+`;
+const Details = styled.div`
+  width: 100%;
+  height:100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0px;
+  padding: 0px 2px;
+`;
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_secondary};
+  overflow: hidden;
+  display: -webkit-box;
+  max-width: 100%;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+const Date = styled.div`
+  font-size: 12px;
+  margin-left: 2px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary + 80};
+  @media only screen and (max-width: 768px) {
+    font-size: 10px;
+  }
+`;
+const Description = styled.div`
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary + 99};
+  text-overflow : hidden;
+  margin-top: 8px;
+  display: -webkit-box;
+  max-width: 100%;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+
+  @media only screen and (max-width: 768px) {
+      overflow: hidden;
+  }
+`;
+const Members = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`;
+const Avatar = styled.img`
+  width: 38px;
+  height: 38px;
+  border-radius: 50%;
+  margin-left: -10px;
+  background-color: ${({ theme }) => theme.white};
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  border: 3px solid ${({ theme }) => theme.card};
+`;
+const Button = styled.a`
+  color: ${({ theme }) => theme.primary};
+  text-decoration: none;
+  font-weight: 600;
+  text-align: center;
+  justify-content: flex-start;
+`;
+
+
+
+
+const ProjectCard = ({ project }) => {
+  return (
+    <CardContainer>
+      <a href={project.webapp}>
+        <Card1>
+          <Image src={project.image}/>
+            <Members>
+                {project.member?.map((member) => (
+                  <Avatar key={member.id} src={member.img}/>
+                ))}
+            </Members>
+          
+        </Card1>
+      </a>
+
+      <Card2>
+        <Details>
+          <Title>{project.title}</Title>
+          <Date>{project.date}</Date>
+
+          <Description>{project.description}</Description>
+        </Details>
+        
+            <Button href={project.github} target = "_blank">
+                View Code
+            </Button>
+
+      </Card2>
+  </CardContainer>
+  );
+};
+
+export default ProjectCard;
---
+++ src/data/constants.js
@@ -0,0 +1,354 @@
+import alaikoo from '../images/Alaikoo.png';
+import animated from '../images/Animated.png';
+import apple from '../images/Apple.png';
+import clock from '../images/Clock.png';
+import face_reco from '../images/Face_Reco.png';
+import xora from '../images/Xora.png';
+import ems from '../images/EMS.png';
+import mb from '../images/MB.png';
+import dsat from '../images/DSAT.png';
+import ielts from '../images/IELTS.png';
+import leetcode from '../images/LeetCode.png';
+import talkzy from "../images/Talkzy.png";
+import netflix from "../images/Netflix.png";
+
+export const assets = {
+    alaikoo,
+    animated,
+    apple,
+    clock,
+    face_reco,
+    xora,
+    leetcode,
+    talkzy,
+    netflix
+}
+
+
+export const Bio = {
+  name: "Mohd Maaz",
+  roles: [
+    "Full Stack Developer",
+    "Software Developer",
+    "Competitive Programmer",
+  ],
+  description:
+    "I'm a software developer with experience in Data Structures & Algorithms (DSA), as well as both frontend and backend development. I also have a good grasp of DevOps and mobile app development, and I'm always working on improving these skills by building new projects. I enjoy tackling complex problems and love experimenting with new technologies to create efficient and well-structured applications.",
+
+  github: "https://github.com/0maaz-01",
+  leetcode : "https://leetcode.com/u/_maaz_1/",
+  linkedin: "https://www.linkedin.com/in/mohammed-maaz-rayeen-b914a4303/",
+  twitter: "https://x.com/1_maaz_1",
+  insta: "https://www.instagram.com/_.maaz._1/?igsh=eG5zeXN1NDhoc25y#",
+};
+
+export const skills = [
+  {
+    title: "My Tech Stack",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://skillicons.dev/icons?i=py",
+      },
+
+      {        name: "HTML",
+        image: "https://skillicons.dev/icons?i=html"
+      },
+
+      {
+        name: "CSS",
+        image:
+          "https://skillicons.dev/icons?i=css",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://skillicons.dev/icons?i=js",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Three Js",
+        image:
+          "https://skillicons.dev/icons?i=threejs",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://skillicons.dev/icons?i=figma",
+
+      },
+
+      {
+        name: "Framer",
+        image:
+          "https://res.cloudinary.com/dgngxuh72/image/upload/v1741131821/nlbvkbmermofz5vo9srj.jpg",
+
+      },
+
+      {
+        name: "React Native",
+        image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Node Js",
+        image: "https://skillicons.dev/icons?i=nodejs",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://skillicons.dev/icons?i=express",
+      },
+      {
+        name: "Postman",
+        image: "https://skillicons.dev/icons?i=postman"
+      },
+      
+      {
+        name: "Graph Ql",
+        image: "https://skillicons.dev/icons?i=graphql",
+      },
+
+      {
+        name: "Fast API",
+        image: "https://skillicons.dev/icons?i=fastapi",
+      },
+
+      {
+        name: "MySQL",
+        image:
+          "https://skillicons.dev/icons?i=mysql",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://skillicons.dev/icons?i=mongodb",
+      },
+
+      {
+        name: "AWS",
+        image:
+          "https://skillicons.dev/icons?i=aws",
+      },
+
+      {
+        name: "Docker",
+        image:
+          "https://skillicons.dev/icons?i=docker",
+      },
+
+      {
+        name: "Kubernetes",
+        image:
+          "https://skillicons.dev/icons?i=kubernetes",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://skillicons.dev/icons?i=vercel",
+      },
+      {
+        name: "Git",
+        image:
+          "https://skillicons.dev/icons?i=git",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://skillicons.dev/icons?i=github",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://skillicons.dev/icons?i=vscode",
+      },
+      {name: "WebStorm",
+      image:
+         "https://skillicons.dev/icons?i=webstorm",
+     },
+     {name: "PyCharm",
+      image: "https://skillicons.dev/icons?i=pycharm",
+     },
+     {name: "Anaconda",
+      image: "https://skillicons.dev/icons?i=anaconda",
+     },
+     {name: "OpenCV",
+      image: "https://skillicons.dev/icons?i=opencv",
+     },
+        
+    ],
+  }]
+
+
+
+
+
+export const education = [
+  {
+    id: 0,
+    img : "https://skillicons.dev/icons?i=js",
+    school: "Full Stack Web Development",
+    date: "Dec 2024 - Present",
+    grade: "null",
+    desc : "Started my journey in web development by learning technologies like HTML, CSS, JavaScript, Express.js, Node.js, and React.js. Applied these skills to build eight complete projects, gaining hands-on experience across the full stack.",
+  },
+  {
+    id: 1,
+    img : "https://skillicons.dev/icons?i=py",
+    school: "Competitive Programming",
+    date: "April 2024 - Present",
+    grade: "null",
+    desc : "Solved over 750 data structure and algorithm problems using Python, laying a strong foundation for machine learning. During this period, I also developed 3 high-quality desktop applications.",
+  },
+  {
+    id: 2,
+    img : ielts,
+    school: "IELTS Exam",
+    date: "Nov 2023 - Dec 2023",
+    grade: "7/9",
+    desc : "Scored 7 out of 9 bands in my first attempt of IELTS within one month of preparation. Listening - 7.5,  Reading - 8.5, Writing - 6, Speaking - 6.5",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 3,
+    img : dsat,
+    school: "Digital SAT Exam",
+    date: "Aug 2023 - Oct 2023",
+    grade: "1420/1600",
+    desc : "Scored 1420/1600 in my first attempt of Digital SAT within two months of preparation and without any external guidance by self study. Reading and Writing - 660, Math - 760",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 4,
+    img : ems,
+    school: "EMMANUEL MISSION SCHOOL, TALWANDI, KOTA",
+    date: "Apr 2021 - July 2022",
+    grade: " 85.00 %",
+    desc: "I completed my class 12 high school education at Emmanuel Mission School, Kota, where I studied Physics, Chemistry and Mathematics as my major.",
+    degree: "CBSE Class XII PCM",
+  },
+  {
+    id: 5,
+    img : mb,
+    school: "MB PUBLIC SR SEC SCH, TALWANDI, KOTA",
+    date: "Apr 2019 - July 2020",
+    grade: " 81.00 %",
+    desc: "I completed my class 10 education at MB Public Senior Secondary School, Kota.",
+    degree: "RBSE Class X",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Netflix Clone",
+    date: "April 2025 - April 2025",
+    description: "Netflix Clone is a fully responsive, feature-rich video streaming web application inspired by the look and feel of Netflix. Designed to provide a smooth user experience, the clone mimics key features of the original platform, including a modern UI, dynamic content fetching, user authentication, and media playback capabilities.",
+    image : netflix,
+    tags: [
+      "React Js",
+      "PostgressSQL",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "React Flow",
+    ],
+    category: "web app",
+    github: "https://github.com/0maaz-01/Apple-Website",
+    webapp: "https://github.com/0maaz-01/Apple-Website",
+  },
+  {
+    id: 9,
+    title: "Talkzy",
+    date: "March 2025 - March 2025",
+    description: "Talkzy is a real-time messaging platform. It offers instant message delivery, and the ability to share media like images. You’ll see when your friends are online or offline, and you can even check message timestamps. It’s designed to look great on both mobile and desktop, with 24 different thems for those who prefer it. Plus, it has secure sign-up/login and a simple, user-friendly interface.",
+    image: talkzy,
+    tags: [
+      "Html",
+      "Css",
+      "JavaSript",
+      "React JS",
+      "Node JS",
+      "Express JS",
+      "MongoDb"
+    ],
+    category: "web app",
+    github: "https://github.com/0maaz-01/Apple-Website",
+    webapp: "https://talkzy.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Apple Website",
+    date: "Feb 2025",
+    description:
+        "A 𝐟𝐫𝐨𝐧𝐭𝐞𝐧𝐝 𝐰𝐞𝐛𝐬𝐢𝐭𝐞 that allows you to explore a detailed 3D model of the iPhone 15 Pro in a visually appealing and interactive experience. This website is fully responsive, providing a seamless experience across all devices. 🌐",
+    image : assets.apple, 
+    tags: ["HTML", "CSS", "JavaScript", "Tailwind CSS", "GSAP", "Three.js", "React JS"],
+    category: "android app",
+    github: "https://github.com/0maaz-01/Apple-Website",
+    webapp: "https://apple-website-alpha-five.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Xora",
+    date: "Feb 2025",
+    description:
+        "Landing page of an AI-Powered Video Editor! This project is a responsive, interactive landing page that showcases the features of a video editing software integrated with artificial intelligence, designed to help users create and edit videos in seconds. This SaaS landing page includes various sections, animations, and interactive elements to provide an engaging user experience.",
+    image : assets.xora,
+    tags: ["HTML", "CSS", "JavaScript", "React.js", "TailwindCSS", "React-Scroll", "clsx", "CountUp", "Slide Down"],
+    category: "android app",
+    github: "https://github.com/0maaz-01/Xora",
+    webapp: "https://xora-wheat-beta.vercel.app/",
+  },
+  {
+    id: 10,
+    title: "Animated Website",
+    date: "Feb 2025",
+    description: "Xentry Website Landing Page Clone is an educational project that replicates the design and functionality of the original Xentry landing page. Perfect for learning front-end development and web design, it features a responsive layout for seamless use across mobile, tablet, and desktop devices, a clean and professional UI, interactive elements like buttons, forms, and smooth animations, plus SEO-friendly optimization with meta tags and proper heading structure.",
+    image: assets.animated,
+    tags: ["React Native", "JavaScript", "Axios"],
+    category: "android app",
+    github: "https://github.com/0maaz-01/Animated-Website",
+    webapp: "https://animated-website-plum.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Alaikoo PDF Reader and Editor",
+    date: "Nov 2024",
+    description:
+      "Alaikoo Text Editor is a powerful and versatile desktop application designed to simplify PDF management with an intuitive interface and advanced features. Whether you're a student, professional, or business owner, Alaikoo provides everything you need to edit, convert, merge, compress, and secure your PDF documents—all in one place.",
+    image : assets.alaikoo,
+    tags: ["React Js", "API", "Axios", "Node JS"],
+    category: "machine learning",
+    github: "https://github.com/0maaz-01/Alaikoo_PDF_Reader_and_Editor.",
+    webapp: "https://github.com/0maaz-01/Alaikoo_PDF_Reader_and_Editor.",
+  },
+  {
+    id: 7,
+    title: "Face Recognition Attendance System",
+    date: "Oct 2024",
+    description: "It is a desktop application that automates attendance tracking using advanced facial recognition technology. The system not only tracks attendance but also maintains a comprehensive database of student details. This includes personal information such as names, student IDs, classes, contact information, etc allowing for easy reference and management.",
+    image: assets.face_reco,
+    tags: ["Python", "MySQL", "OpenCV", "Pillow", "Numpy", "MySQL Connector", "Tkinter", "OS", "DateTime", "Numpy", "Pillow", "CSV", "Cx_Freeze", "Sys"],
+    category: "machine learning",
+    github: "https://github.com/0maaz-01/Face-Recognition-Attendance-System",
+    webapp: "https://github.com/0maaz-01/Face-Recognition-Attendance-System",
+  },
+  {
+    id: 8,
+    title: "Clock App",
+    date: "Dec 2024",
+    description:
+      "This application is a multifunctional clock that provides several features such as a timer, stopwatch, world clock, and a digital clock with city-specific time zones. The app is built using Tkinter and CustomTkinter for its graphical interface and functionality. It allows users to track time in multiple ways and provides a sleek, modern look.",
+    image: assets.clock,
+    tags: ["Python", "CustomTkinter", "pytz", "timezonefinder", "datetime", "Time", "Math"],
+    category: "machine learning",
+    github: "https://github.com/0maaz-01/Clock",
+    webapp: "https://github.com/0maaz-01/Clock",
+  },
+];
