24a079170243883161df2e4bc94b874cc8a545c4
1maaz01 <mmrayeen401@gmail.com>
2025-04-13 06:09:53 +0530

    Update Hero.jsx

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -6,7 +6,6 @@ import HeroImg from "../../images/HeroImage.jpg";
 import { motion } from "framer-motion";
 import {
   headContainerAnimation,
-  headContentAnimation,
   headTextAnimation,
 } from "../../utils/motion";
 
@@ -14,9 +13,12 @@ import {
 const HeroContainer = styled.div`
   display: flex;
   justify-content: center;
+  min-width : 100vw;
+  min-height: 100vh;
   position: relative;
   padding: 80px 30px;
   z-index: 1;
+
 
   @media (max-width: 960px) {
     padding: 66px 16px;
@@ -31,10 +33,15 @@ const HeroContainer = styled.div`
 const HeroInnerContainer = styled.div`
   position: relative;
   display: flex;
-  justify-content: space-between;
+
   align-items: center;
-  width: 100%;
-  max-width: 1100px;
+  width: 100vw;
+
+  justify-content: center; /* Horizontal alignment */
+  max-width: 1200px;
+  height: ; 
+
+
 
   @media (max-width: 960px) {
     flex-direction: column;
@@ -43,6 +50,7 @@ const HeroInnerContainer = styled.div`
 const HeroLeftContainer = styled.div`
   width: 100%;
   order: 1;
+
   @media (max-width: 960px) {
     order: 2;
     margin-bottom: 30px;
@@ -53,6 +61,7 @@ const HeroLeftContainer = styled.div`
   }
 `;
 const HeroRightContainer = styled.div`
+
   width: 100%;
   order: 2;
   display: flex;
@@ -93,7 +102,7 @@ const TextLoop = styled.div`
   font-size: 32px;
   display: flex;
   gap: 12px;
-  color: ${({ theme }) => theme.text_primary};
+  color: rgb(244, 244, 244);
   line-height: 68px;
 
   @media (max-width: 960px) {
@@ -109,7 +118,7 @@ const TextLoop = styled.div`
 
 const Span = styled.div`
   cursor: pointer;
-  color: ${({ theme }) => theme.primary};
+  color: rgb(173, 5, 245);
 `;
 
 const SubTitle = styled.div`
@@ -137,34 +146,22 @@ const ResumeButton = styled.a`
   width: 95%;
   max-width: 300px;
   text-align: center;
-  padding: 16px 0;
 
-  background: hsla(271, 100%, 50%, 1);
-  background: linear-gradient(
-    225deg,
-    hsla(271, 100%, 50%, 1) 0%,
-    hsla(294, 100%, 50%, 1) 100%
-  );
-  background: -moz-linear-gradient(
-    225deg,
-    hsla(271, 100%, 50%, 1) 0%,
-    hsla(294, 100%, 50%, 1) 100%
-  );
-  background: -webkit-linear-gradient(
-    225deg,
-    hsla(271, 100%, 50%, 1) 0%,
-    hsla(294, 100%, 50%, 1) 100%
-  );
-  box-shadow: 20px 20px 60px #1f2634, -20px -20px 60px #1f2634;
+  padding: 16px 0;
+  border : 2px solid rgb(231, 223, 221);
+
+  background: rgb(36, 37, 30);;
+
   border-radius: 50px;
+  
   font-weight: 600;
   font-size: 20px;
+  cursor: pointer;
 
-     &:hover {
+    &:hover {
         transform: scale(1.05);
-    transition: all 0.4s ease-in-out;
-    box-shadow:  20px 20px 60px #1F2634,
-    filter: brightness(1);
+        transition: all 0.4s ease-in-out;
+        filter: brightness(1);
     }    
     
     
@@ -175,44 +172,24 @@ const ResumeButton = styled.a`
     color: white;
 `;
 
+// rgb(95, 0, 136)
+
 const Img = styled.img`
   border-radius: 50%;
   width: 100%;
   height: 100%;
   max-width: 400px;
   max-height: 400px;
-  border: 8px solid purple;
+  border: 8px solid rgb(74, 0, 106);
+
 
   @media (max-width: 640px) {
     max-width: 280px;
     max-height: 280px;
-    border: 4px solid purple;
+    border: 4px solid rgb(231, 223, 221);
   }
 `;
 
-const HeroBg = styled.div`
-  position: absolute;
-  display: flex;
-  justify-content: end;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  max-width: 1360px;
-  overflow: hidden;
-  padding: 0 30px;
-  top: 50%;
-  left: 50%;
-  -webkit-transform: translateX(-50%) translateY(-50%);
-  transform: translateX(-50%) translateY(-50%);
-
-  @media (max-width: 960px) {
-    justify-content: center;
-    padding: 0 0px;
-  }
-`;
 
 const Hero = () => {
   return (
@@ -240,18 +217,16 @@ const Hero = () => {
                 </TextLoop>
               </motion.div>
 
-              <motion.div {...headContentAnimation}>
+
                 <SubTitle>{Bio.description}</SubTitle>
-              </motion.div>
+
 
               <ResumeButton href={Bio.resume} target="_blank">
                 Check Resume
               </ResumeButton>
             </HeroLeftContainer>
             <HeroRightContainer>
-              <motion.div {...headContentAnimation}>
-                  <Img src={HeroImg} alt="Mohammed Maaz Rayeen" />
-              </motion.div>
+                  <Img src={HeroImg} alt="Mohd Maaz" />
             </HeroRightContainer>
           </HeroInnerContainer>
         </motion.div>
